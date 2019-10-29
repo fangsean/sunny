f4b61387b286261818f9117614402eb9a8ebc19f
jsen.yin <jsen.yin@gmail.com>
2019-10-29 14:39:39 +0800

    log

--- src/main/java/com/shu/shuny/test/HelloServiceImpl.java
+++ src/main/java/com/shu/shuny/test/HelloServiceImpl.java
@@ -1,14 +1,17 @@
 package com.shu.shuny.test;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service("helloService")
 public class HelloServiceImpl implements HelloService {
+    private static final Logger logger = LoggerFactory.getLogger(HelloServiceImpl.class);
 
     @Override
     public String sayHello(String msg) {
-        System.out.println("provider: hello " + msg + "!");
+        logger.info("provider: hello " + msg + "!");
         return "hello " + msg + "!";
     }
 
--- src/main/java/com/shu/shuny/test/IndexServiceImpl.java
+++ src/main/java/com/shu/shuny/test/IndexServiceImpl.java
@@ -1,13 +1,16 @@
 package com.shu.shuny.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service("indexService")
 public class IndexServiceImpl implements IndexService {
+    private static final Logger logger = LoggerFactory.getLogger(IndexServiceImpl.class);
 
     @Override
     public String index(String msg) {
-        System.out.println("provider: index " + msg + "!");
+        logger.info("provider: index " + msg + "!");
         return "index " + msg + "!";
     }
 
